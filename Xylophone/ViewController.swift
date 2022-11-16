95ef5a57fe5e980be55f574c49e32763eed5bdce
Chukwubuikem Chukwudi <myecodevelopment@gmail.com>
2022-11-16 20:19:26 +0100

    Initial commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.layer.opacity = 0.4
+        playSound(soundName: sender.currentTitle!)
+//        sender.layer.opacity = 1
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
+
